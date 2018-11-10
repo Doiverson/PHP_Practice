c156faba67b12f8103694d84085589a801fad71d
Shosuke Doi <doiverson.canada@gmail.com>
2018-11-10 13:58:44 -0800

    add php practice

---
+++ strings.php
@@ -0,0 +1,16 @@
+<?php
+$exercise1 = 'A';
+$exercise2 = 'B';
+$exercise3 = 'C';
+$exercise4 = 'D';
+$exercise5 = 'E';
+$exercise6 = 'F';
+$exercise7 = 'G';
+
+
+/**
+ * Created by PhpStorm.
+ * User: doiverson
+ * Date: 2018-11-10
+ * Time: 1:03 PM
+ */
