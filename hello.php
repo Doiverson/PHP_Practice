a55457c079bb306ad31739f742f0c1f6cad36daa
Shosuke Doi <doiverson.canada@gmail.com>
2018-11-10 17:31:58 -0800

    update

--- hello.php
+++ hello.php
@@ -1,10 +1,19 @@
-<?php
-    $num_one = 1;
-    $num_two = 2;
-    $num_three = 3;
+<?php 
+/*
+ * This file is our first "Hello World!" script
+ * 
+ * @author: Alena Holligan
+*/
+//our first php script
+echo 'Hello World!'; //done
+//echo 'Hello Treehouse!'; //done
 
-    var_dump( $num_one );
-    var_dump(1);
-    var_dump("1");
-    var_dump($num_one + $num_three);
+//you can make a single line comment as long as you want, even wrapping to a new line, as long as you don't add a  hard return
+//This is a second comment line
+
+/*
+line 1
+line 2
+line 3
+*/
 ?>
--- strings.php
+++ strings.php
@@ -1,16 +1,35 @@
 <?php
-$exercise1 = 'A';
-$exercise2 = 'B';
-$exercise3 = 'C';
-$exercise4 = 'D';
-$exercise5 = 'E';
-$exercise6 = 'F';
-$exercise7 = 'G';
+$name = 'Alena';
+$string_one = "Learning to display \"Hello $name!\" to the screen.\n";
+$string_one = 'Learning to display ';
+$string_one .= '"Hello ';
+$string_one .= $name;
+$string_one .= '!" to the screen.';
+//$string_one = $string_one . "\n";
+//prepend to a string
+//$string_one = 'I am ' . $string_one;
+//echo $string_one;
 
+$isReady = true;
+//var_dump($isReady);
+$isReady = false;
+//var_dump($isReady);
 
-/**
- * Created by PhpStorm.
- * User: doiverson
- * Date: 2018-11-10
- * Time: 1:03 PM
- */
+//var_dump(1 + "2");
+
+$a = 10;
+$b = '10';
+
+//var_dump($a == $b);
+//var_dump($a === $b);
+
+//($string_one == 'Learning to display "Hello Alena!" to the screen.');
+$string_one = "";
+if ($string_one == 'Learning to display "Hello Alena!" to the screen.') {
+ echo 'the values match';  
+} elseif ($string_one == "") {
+    echo '$string_one is empty';
+} else {
+ echo 'the values DO NOT match';   
+}
+?>
