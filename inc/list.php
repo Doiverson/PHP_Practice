bf8520299169d3f3e6fd1df7c0dbabe494456325
Shosuke Doi <doiverson.canada@gmail.com>
2018-11-29 17:36:13 -0800

    add foreach

---
+++ inc/list.php
@@ -0,0 +1,15 @@
+<?php
+$list[] = [
+    'title' => 'Laundry',
+    'priority' => 2,
+    'due' => '',
+    'complete' => false,
+];
+
+$list[] = [
+    'title' => 'Clean out refrigerator',
+    'priority' => 3,
+    'due' => '07/30/2016',
+    'complete' => false,
+];
+
---
+++ inc/todo.php
@@ -0,0 +1,30 @@
+<?php
+include 'list.php';
+
+foreach ($list as $key => $item) {
+    echo $key . ' = ' . $item['title'] . "\n";
+}
+
+echo '<table>';
+echo '<tr>';
+echo '<th>Title</th>';
+echo '<th>Priority</th>';
+echo '<th>Due Date</th>';
+echo '<th>Complete</th>';
+echo '</tr>';
+
+foreach ($list as $item) {
+    echo '<tr>';
+    echo '<td>' . $item['title'] . "</td>\n";
+    echo '<td>' . $item['priority'] . "</td>\n";
+    echo '<td>' . $item['due'] . "</td>\n";
+    echo '<td>';
+    if ($item['complete']) {
+        echo 'Yes';
+    } else {
+        echo 'No';
+    }
+    echo "</td>\n";
+    echo '</tr>';
+}
+echo '</table>';
