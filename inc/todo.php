69ca39a6c2567a238325a1d123fa9edb309ffc3a
Shosuke Doi <doiverson.canada@gmail.com>
2018-11-30 14:59:15 -0800

    sooo sleepy...

--- inc/todo.php
+++ inc/todo.php
@@ -1,9 +1,16 @@
 <?php
 include 'list.php';
 
+$status = 'false';
+$filter = array();
 foreach ($list as $key => $item) {
-    echo $key . ' = ' . $item['title'] . "\n";
+    if ($status === 'all' || !$item['complete'] == $status) {
+        $filter[] = $key;
+    }
 }
+
+echo '<pre>';
+var_dump($filter, boolval('all'), $status == 'all');
 
 echo '<table>';
 echo '<tr>';
@@ -14,17 +21,17 @@ echo '<th>Complete</th>';
 echo '</tr>';
 
 foreach ($list as $item) {
-    echo '<tr>';
-    echo '<td>' . $item['title'] . "</td>\n";
-    echo '<td>' . $item['priority'] . "</td>\n";
-    echo '<td>' . $item['due'] . "</td>\n";
-    echo '<td>';
-    if ($item['complete']) {
-        echo 'Yes';
-    } else {
-        echo 'No';
-    }
-    echo "</td>\n";
-    echo '</tr>';
+        echo '<tr>';
+        echo '<td>' . $item['title'] . "</td>\n";
+        echo '<td>' . $item['priority'] . "</td>\n";
+        echo '<td>' . $item['due'] . "</td>\n";
+        echo '<td>';
+        if ($item['complete']) {
+            echo 'Yes';
+        } else {
+            echo 'No';
+        }
+        echo "</td>\n";
+        echo '</tr>';
 }
 echo '</table>';
